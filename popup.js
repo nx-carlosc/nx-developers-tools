dd1716a87a20f9770fc89860cb06bec57bddd9ea
carlosc <carlos.company@nexum.com>
2022-08-21 14:32:54 +0200

    Cookie is array of objects

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+main()
+
 async function getCurrentTab() {
   return new Promise((resolve, reject) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -11,25 +13,34 @@ async function getCurrentTab() {
 }
 
 async function main() {
-  const domainInput = document.querySelector("[name=domain]")
+  const COOKIE_NAME = "domain-replacer-values"
+  const domainInput = document.querySelector("#replaceDomainInput")
   const replaceButton = document.querySelector("#replaceDomainButton")
+  let domainsCookie = []
+  const rawCookie = getCookie(COOKIE_NAME)
 
-  domainInput.addEventListener("input", function (e) {
-    const value = e.target.value
-    setCookie("domain-replacer-value", value)
-  })
-  
+  if (rawCookie) {
+    domainsCookie = JSON.parse(rawCookie)
+  }
+
+  domainsCookie.length > 0 &&
+    (domainInput.value = domainsCookie[domainsCookie.length - 1].name)
+
   replaceButton.addEventListener("click", async () => {
-    const cookie = getCookie("domain-replacer-value")
+    const domain = domainInput.value
 
-    if (cookie) {
+    if (domain.trim()) {
+      if (!domainsCookie.every((el) => el.name !== domain)) {
+        const newDomains = [
+          ...domainsCookie,
+          { name: domain, createdAt: Date.now() },
+        ]
+        domainsCookie = newDomains
+        setCookie(COOKIE_NAME, JSON.stringify(newDomains))
+      }
+
       const activeTab = await getCurrentTab()
-      chrome.tabs.sendMessage(activeTab.id, { newDomain: cookie })
+      chrome.tabs.sendMessage(activeTab.id, { newDomain: domain })
     }
   })
-
-  const cookie = getCookie("domain-replacer-value")
-  cookie && (domainInput.value = cookie)
 }
-
-main()

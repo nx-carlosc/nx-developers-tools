b7cec6b68fdf4257bf90f7a058d3262fc4e7944f
carlosc <carlos.company@nexum.com>
2022-08-21 13:21:20 +0200

    Refactor popup.js

--- popup.js
+++ popup.js
@@ -1,19 +1,35 @@
-const domainInput = document.querySelector("[name=domain]")
-const replaceButton = document.querySelector("#replaceDomainButton")
-
-domainInput.addEventListener("input", function (e) {
-  const value = e.target.value
-  setCookie("domain-replacer-value", value)
-})
-
-replaceButton.addEventListener("click", function () {
-  const cookie = getCookie("domain-replacer-value")
-  if (cookie) {
+async function getCurrentTab() {
+  return new Promise((resolve, reject) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      tabs[0] && chrome.tabs.sendMessage(tabs[0].id, { newDomain: cookie })
+      if (tabs.length) {
+        resolve(tabs[0])
+      } else {
+        reject(new Error("No active tab"))
+      }
     })
-  }
-})
+  })
+}
 
-const cookie = getCookie("domain-replacer-value")
-cookie && (domainInput.value = cookie)
+async function main() {
+  const domainInput = document.querySelector("[name=domain]")
+  const replaceButton = document.querySelector("#replaceDomainButton")
+
+  domainInput.addEventListener("input", function (e) {
+    const value = e.target.value
+    setCookie("domain-replacer-value", value)
+  })
+  
+  replaceButton.addEventListener("click", async () => {
+    const cookie = getCookie("domain-replacer-value")
+
+    if (cookie) {
+      const activeTab = await getCurrentTab()
+      chrome.tabs.sendMessage(activeTab.id, { newDomain: cookie })
+    }
+  })
+
+  const cookie = getCookie("domain-replacer-value")
+  cookie && (domainInput.value = cookie)
+}
+
+main()

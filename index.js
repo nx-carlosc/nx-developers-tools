d4b310fa18af675d32f9ccc2e44ac66d926c4263
carlosc <carlos.company@nexum.com>
2022-08-21 13:19:13 +0200

    Changed file name

--- index.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-  const { newDomain } = message
-  const href = new URL(window.location.href)
-  const pathname = href.pathname
-  const newUrl = `${newDomain}${pathname}`
-  window.location.href = newUrl
-})

db76d2dc176dbd351535b0b1ae798f348093b337
carlosc <carlos.company@nexum.com>
2022-09-16 16:08:47 +0200

    store domains in storage

--- popup.js
+++ popup.js
@@ -1,19 +1,31 @@
-const COOKIE_NAME = "domain-replacer-values"
+const STORED_DOMAINS_NAME = "domain-replacer-values"
 const domainInput = document.querySelector("#replaceDomainInput")
 const replaceButton = document.querySelector("#replaceDomainButton")
 const removeButton = document.querySelector("#removeDomainButton")
-const rawCookie = getCookie(COOKIE_NAME)
-
-if (rawCookie) {
-	const domainsCookie = JSON.parse(rawCookie)
-	fillWithCookieDomains(domainsCookie)
-	domainInput.select()
-}
-
 replaceButton.addEventListener("click", handleClickReplaceDomain)
 removeButton.addEventListener("click", handleClickRemoveDomain)
 
-function fillWithCookieDomains(domains) {
+getStoredDomainsAndFillInput()
+domainInput.select()
+
+function getStoredDomainsAndFillInput(newDomain) {
+	chrome.storage.sync.get([STORED_DOMAINS_NAME], (result) => {
+		const storedDomains = result[STORED_DOMAINS_NAME] || []
+		const newDomains = newDomain
+			? storedDomains.every((el) => el.name !== selectedDomain)
+				? [...storedDomains, newDomain]
+				: [
+						...storedDomains.filter((el) => el.name !== selectedDomain),
+						newDomain,
+				  ]
+			: storedDomains
+
+		fillWithStoredDomains(newDomains)
+		chrome.storage.sync.set({ [STORED_DOMAINS_NAME]: newDomains })
+	})
+}
+
+function fillWithStoredDomains(domains) {
 	domains.sort((a, b) => b.createdAt - a.createdAt)
 	const datalist = document.querySelector("datalist#domains")
 	domainInput.value = domains.length ? domains[0].name : ""
@@ -21,7 +33,8 @@ function fillWithCookieDomains(domains) {
 	const options = domains
 		.map(
 			(domain, i) =>
-				`<option ${i === 0 ? "selected" : ""} value="${domain.name}">${domain.name
+				`<option ${i === 0 ? "selected" : ""} value="${domain.name}">${
+					domain.name
 				}</option>`
 		)
 		.join("\n")
@@ -31,17 +44,13 @@ function fillWithCookieDomains(domains) {
 
 async function handleClickReplaceDomain(event) {
 	event.preventDefault()
-	const selectedDomain = domainInput.value.trim().replace(/\/$/, '')
+	const selectedDomain = domainInput.value.trim().replace(/\/$/, "")
+
+	console.log({ selectedDomain });
 
 	if (selectedDomain) {
 		const newDomain = { name: selectedDomain, createdAt: Date.now() }
-		const rawCookie = getCookie(COOKIE_NAME)
-		const domainsCookie = rawCookie ? JSON.parse(rawCookie) : []
-		const newDomains = domainsCookie.every((el) => el.name !== selectedDomain)
-			? [...domainsCookie, newDomain]
-			: [...domainsCookie.filter((el) => el.name !== selectedDomain), newDomain]
-		setCookie(COOKIE_NAME, JSON.stringify(newDomains))
-		fillWithCookieDomains(newDomains)
+		getStoredDomainsAndFillInput(newDomain)
 		const activeTab = await getCurrentTab()
 		const tabUrl = activeTab.url
 
@@ -49,18 +58,18 @@ async function handleClickReplaceDomain(event) {
 			const { origin } = new URL(tabUrl)
 			const newUrl = tabUrl.replace(origin, selectedDomain)
 			const $message = document.querySelector(".message")
-			$message.innerHTML = 'Fetching...'
+			$message.innerHTML = "Fetching..."
 
 			fetch(newUrl, {
-				mode: 'no-cors'
+				mode: "no-cors",
 			})
-			.then(async () => {
-				$message.innerHTML = ''
-				chrome.tabs.create({ url: newUrl })
-			})
-			.catch(() => {
-				$message.innerHTML = "No available domain"
-			})
+				.then(() => {
+					$message.innerHTML = ""
+					chrome.tabs.create({ url: newUrl })
+				})
+				.catch(() => {
+					$message.innerHTML = "No available domain"
+				})
 		}
 	}
 }
@@ -68,18 +77,18 @@ async function handleClickReplaceDomain(event) {
 function handleClickRemoveDomain(event) {
 	event.preventDefault()
 	const selectedDomain = domainInput.value
-	const rawCookie = getCookie(COOKIE_NAME)
-	if (rawCookie) {
-		const domainsCookie = rawCookie ? JSON.parse(rawCookie) : []
-		const newDomains = domainsCookie.filter(
+	chrome.storage.sync.get([STORED_DOMAINS_NAME], (result) => {
+		const storedDomains = result[STORED_DOMAINS_NAME] || []
+
+		const newDomains = storedDomains.filter(
 			(domain) => domain.name !== selectedDomain
 		)
-		setCookie(COOKIE_NAME, JSON.stringify(newDomains))
-		fillWithCookieDomains(newDomains)
-	}
+		chrome.storage.sync.set({ [STORED_DOMAINS_NAME]: newDomains })
+		fillWithStoredDomains(newDomains)
+	})
 }
 
-async function getCurrentTab() {
+function getCurrentTab() {
 	return new Promise((resolve, reject) => {
 		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 			if (tabs.length) {
